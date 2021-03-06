2e17bc7511ad039bf3d7f653104dd5f3a0a98d35
JahanzebNawaz <jahanzaib.ch9996@gmail.com>
2021-03-06 23:39:32 +0500

    Solidity assignment 1

--- assignment4_PIAIC100930/task1.js
+++ assignment4_PIAIC100930/task1.js
@@ -1,12 +1,6 @@
-// Write a program to replace the “Hyder” to “Islam” in the word“Hyderabad” and display the result in your browser
+var a = 0
 
-
-let city = "Hyderabad";
-
-document.write(` <b>City: ${city} </b> <br> `);
-
-let city_2 = city.replace('Hyder', 'Islam')
-
-document.write(`<b>City: ${city_2} </b>`);
-
-
+if(4%2){
+    a++;
+}
+alert(a)
