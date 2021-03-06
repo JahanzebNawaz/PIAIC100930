0aa1e60a73f1286147c8b0db270db5557bcba716
JahanzebNawaz <jahanzaib.ch9996@gmail.com>
2021-03-06 23:47:42 +0500

    redundent code remove

--- assignment5_PIAIC100930/task1.sol
+++ assignment5_PIAIC100930/task1.sol
@@ -2,6 +2,7 @@ pragma solidity ^0.5.0;
 
 contract Address{
     address payable private myAddress = 0xdD870fA1b7C4700F2BD7f44238821C26f7392148;
+    
     uint NoOfStudents;
     mapping (uint => StudentStruct)  Students;
     mapping(string => mapping(uint => StudentStruct)) Class;
@@ -19,26 +20,38 @@ contract Address{
     }
 
     
-    // function to create new students.
-    // function setStudent(uint _id, string memory _name, bool _hasDegree) public {
-    //     Students[_id] = StudentStruct({name: _name, ethAddress: msg.sender, hasDegree: _hasDegree});
-    // }
-    
-    // function to get Stuck created students.
-    // function getStudent(uint _id) public view returns (string memory, address, bool){
-    //     return (Students[_id].name, Students[_id].ethAddress, Students[_id].hasDegree);
-    // }
     
     // function to create new students.
-    function enrollStudentInClass(string memory _class, uint _id, string memory _name, bool _hasDegree, Gender _gender, ClassType _classtype) public payable {
-        //require(msg.value > 2, "Not enough balance");
-        Class[_class][_id] = StudentStruct({name: _name, ethAddress: msg.sender,hasDegree: _hasDegree, gender: _gender, classtype: _classtype});
+    function enrollStudentInClass(
+        string memory _class, uint _id, string memory _name,
+            bool _hasDegree, Gender _gender, ClassType _classtype) public payable {
+
+        require(_hasDegree == true, "You must have BS degree");
+        require(msg.value == 2 ether, "You must pay 2 Ether for Registration");
+
+        Class[_class][_id] = StudentStruct({
+            name: _name, 
+            ethAddress: msg.sender,
+            hasDegree: _hasDegree, 
+            gender: _gender, 
+            classtype: _classtype});
+
         NoOfStudents += 1;
+        myAddress.transfer(msg.value);
     }
     
     // function to get Stuck created students.
-    function getEnrolledStudent(uint _id, string memory _class) public view returns (string memory, address, bool, Gender, ClassType){
-        return (Class[_class][_id].name, Class[_class][_id].ethAddress, Class[_class][_id].hasDegree, Class[_class][_id].gender, Class[_class][_id].classtype);
+    function getEnrolledStudent(uint _id, string memory _class) public view returns (
+        string memory, address, bool, Gender, ClassType)
+    {
+        
+        return (
+            Class[_class][_id].name,
+            Class[_class][_id].ethAddress,
+            Class[_class][_id].hasDegree,
+            Class[_class][_id].gender,
+            Class[_class][_id].classtype
+            );
     }
     
     function getNoOfStudents () public view returns (uint){
