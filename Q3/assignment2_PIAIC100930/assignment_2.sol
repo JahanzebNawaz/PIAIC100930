1e7b230f78d811351c918e8e6ffe588e55ed979b
Jahanzaib Nawaz <jahanzaib.ch9996@gmail.com>
2021-08-01 01:30:40 +0500

    bank account code added

--- Q3/assignment2_PIAIC100930/assignment_2.sol
+++ Q3/assignment2_PIAIC100930/assignment_2.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.8.6;
+pragma solidity 0.8.0;
 
 // Create Crypto Bank Contract
 
@@ -15,14 +15,64 @@ pragma solidity ^0.8.6;
 contract Bank {
     
     address public owner;
+    mapping(address => uint) private accounts;
+    uint counter;
+    // struct Account {
+    //     string name;
+    //     uint balance;
+    // }
+    // Account account;
     
     constructor () payable {
         owner = msg.sender;
-        require(msg.value >= 2**18, "Initial Value should be Greater or equals to 50 Ether!");
+        require(msg.value >= 50**18, "Initial Value should be Greater or equals to 50 Ether!");
+        counter = 0;
     }
     
-    function closeBank() public{
-        require(msg.sender == owner; "Only owner can close Bank!");
+    modifier validAccount() {
+        require(
+            msg.sender != address(0) && msg.value >= 2**18, 
+            "Valid Address and Initial balance should be more than 1 Ether!"
+            );
+            _;
+    }
+    
+    function closeBank() external payable{
+        require(msg.sender == owner, "Only owner can close Bank!");
         selfdestruct(payable(owner));
+    }
+    
+    function openAccount() public payable validAccount{
+        accounts[msg.sender] = msg.value;
+        if(counter <= 4){
+            payable(msg.sender).transfer(1**18);
+            accounts[msg.sender] += 1**18;
+            counter++;
+        }
+    }
+    
+    function depositAmount(address _sender, uint _amount) public payable validAccount{
+        accounts[_sender] = _amount;
+    }
+    
+    function withdrawAmount(uint _amount) public payable validAccount{
+        require(_amount >= accounts[msg.sender], "Not enough balance!");
+        payable(msg.sender).transfer(_amount);
+        accounts[msg.sender] -= _amount;
+    }
+    
+    function getBalance() public view returns(uint){
+        return accounts[msg.sender];
+    }
+    
+    function closeAccount() public  {
+        require(msg.sender != address(0) && accounts[msg.sender] > 0, "Invalid Account Address!");
+        payable(msg.sender).transfer(accounts[msg.sender]);
+        delete accounts[msg.sender];
+    }
+    
+    function bankBalance() public view returns(uint){
+        require(msg.sender == owner, "Only owner can view");
+        return address(this).balance;
     }
 }
